5730329e100394374d6e59decadd5cd180820917
MSchiborr <mark.schiborr@student.uibk.ac.at>
2016-04-03 20:47:44 +0200

    Versuch Multiserver - leider nicht erfolgreich

--- HW_2/src/ex2_4/HandleRequest.java
+++ HW_2/src/ex2_4/HandleRequest.java
@@ -8,7 +8,7 @@ import ex2_3.Protocol;
 public class HandleRequest extends Thread {
 	private Socket connection;
 
-	public void RequestHandlerThread(Socket connection) {
+	public HandleRequest(Socket connection) {
 		this.connection = connection;
 		System.out.println("Connection received from " + connection.getInetAddress().getHostName());
 	}
@@ -16,21 +16,21 @@ public class HandleRequest extends Thread {
 	@Override
 	public void run() {
 		Protocol protocol = new Protocol();
-		
+
 		try {
 			protocol.InitServer(connection);
-			
+
 			boolean socketOpen = true;
 
 			// Go for as long as the client is not closed
-			while(socketOpen) {
+			while (socketOpen) {
 				socketOpen = protocol.reply();
 			}
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 }
--- HW_2/src/ex2_4/MultiServer.java
+++ HW_2/src/ex2_4/MultiServer.java
@@ -4,60 +4,49 @@ import java.io.*;
 import java.net.*;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
 
 import ex2_3.Protocol;
 
-public class MultiServer{
+public class MultiServer {
 
-    //TODO: ExecutorService only works for one thread at a time...
+	// TODO: ExecutorService only works for one thread at a time...
 
 	private static ServerSocket providerSocket;
+	private static LinkedBlockingQueue<Runnable> runnables = new LinkedBlockingQueue<Runnable>();
+	private static final ThreadPoolExecutor executor = new ThreadPoolExecutor(5, 100, 100, TimeUnit.MILLISECONDS,
+			runnables);
 
 	public static void main(String[] args) {
-		ExecutorService executor = Executors.newCachedThreadPool();
 
 		// Add a hook for shutdown exception handling
 		// NOTE: Hook does not work inside Eclipse
-        // must send a TERM signal with kill
+		// must send a TERM signal with kill
 		/*
-		Runtime.getRuntime().addShutdownHook(new Thread()
-        {
-            @Override
-            public void run()
-            {
-                System.out.println("Shutdown hook activated!");
-                System.out.println("Shutting Server down..");
-                try {
-                	connection.close();
-                } catch (IOException e) {
-                	System.out.println("Could not close server socket.");
-                	e.printStackTrace();
-                } catch (NullPointerException e) {
-                	System.out.println("No connections found.");
-            	} finally {
-	                executor.shutdown();
-	                // Wait for all threads to finish
-	    			while (!executor.isTerminated()) {
-	    				System.out.print(".");
-	    			}
-                }
-            }
-        });
-        */
+		 * Runtime.getRuntime().addShutdownHook(new Thread() {
+		 * 
+		 * @Override public void run() { System.out.println(
+		 * "Shutdown hook activated!"); System.out.println(
+		 * "Shutting Server down.."); try { connection.close(); } catch
+		 * (IOException e) { System.out.println("Could not close server socket."
+		 * ); e.printStackTrace(); } catch (NullPointerException e) {
+		 * System.out.println("No connections found."); } finally {
+		 * executor.shutdown(); // Wait for all threads to finish while
+		 * (!executor.isTerminated()) { System.out.print("."); } } } });
+		 */
 
 		// Server logic
 		try {
 			providerSocket = new ServerSocket();
-            providerSocket.setReuseAddress(true);
-            providerSocket.bind(new InetSocketAddress(Protocol.getPortNumber()), 10);
+			providerSocket.setReuseAddress(true);
+			providerSocket.bind(new InetSocketAddress(Protocol.getPortNumber()), 10);
 
 			while (true) {
 				System.out.println("Waiting for connections...");
 
-				HandleRequest newRequest = new HandleRequest();
-				newRequest.RequestHandlerThread(providerSocket.accept());
-
-				executor.execute(newRequest);
+				executor.execute(new HandleRequest(providerSocket.accept()));
 			}
 		} catch (IOException e) {
 			System.out.println("Could not establish a connection!");
